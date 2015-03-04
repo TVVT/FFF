fcc4cc051c8236d3dc28041a6bec53c32ab52305
kevin14 <kevin14@me.com>
2015-03-04 16:00:44 +0800

    删除messageCenter文件 并且使用了94服务器绝对地址;新增了basecss

--- yhd_mxc.js
+++ yhd_mxc.js
@@ -1,11 +1,11 @@
 require.config({
     paths: {
-        base: './core/base',
-        attribute: './core/attribute',
-        FFF: './core/FFF',
-        zepto: './lib/zepto',
-        language: './core/language',
-        eventEmitter: './core/eventEmitter',
-        widget: './core/widget'
+        base: 'http://192.168.112.94/fff/core/base',
+        attribute: 'http://192.168.112.94/fff/core/attribute',
+        FFF: 'http://192.168.112.94/fff/core/FFF',
+        zepto: 'http://192.168.112.94/fff/lib/zepto',
+        language: 'http://192.168.112.94/fff/core/language',
+        eventEmitter: 'http://192.168.112.94/fff/core/eventEmitter',
+        widget: 'http://192.168.112.94/fff/core/widget'
     }
 });
