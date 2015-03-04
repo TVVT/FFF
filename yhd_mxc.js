e88653aa7aa6f406cac1661c23b6be23e0af44a7
kevin14 <kevin14@me.com>
2015-03-04 15:49:32 +0800

    删除demo以及测试目录和文件

---
+++ yhd_mxc.js
@@ -0,0 +1,11 @@
+require.config({
+    paths: {
+        base: './core/base',
+        attribute: './core/attribute',
+        FFF: './core/FFF',
+        zepto: './lib/zepto',
+        language: './core/language',
+        eventEmitter: './core/eventEmitter',
+        widget: './core/widget'
+    }
+});
